a225231d93083a111edc039b94b84063df4d704d
cheese <deanlemans5646@gmail.com>
2024-10-23 10:32:25 +0200

    created working win scene

--- DDW-2024/Assets/Scripts/MainMenu.cs
+++ DDW-2024/Assets/Scripts/MainMenu.cs
@@ -7,12 +7,18 @@ public class MainMenu : MonoBehaviour
     public void Play() 
     {
         SceneManager.LoadScene("PVP");
-        Debug.Log("utton pressed"); 
+        Debug.Log("the entity know as player has begun playing the game"); 
     }
 
     public void Quit()
     {
         Application.Quit();
         Debug.Log("the entity know as player has quit the game");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("main menu");
+        Debug.Log("the entity know as player has restarted the game");
     }
 }
